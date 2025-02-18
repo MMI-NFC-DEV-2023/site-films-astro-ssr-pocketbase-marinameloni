fa7f28eccb9267da11b2f6ec1ef2e4ad2d87ac56
marinameloni <marina.meloni@edu.univ-fcomte.fr>
2025-02-18 14:45:03 +0100

    ajout de la gestion de l'URL de PocketBase via les variables d'environnement et mise à jour des appels dans le middleware et les pages

--- src/backend.ts
+++ src/backend.ts
@@ -1,4 +1,6 @@
-import PocketBase from 'pocketbase'
-import type { TypedPocketBase } from './pocketbase-typegen'
+import PocketBase from "pocketbase";
+import type { TypedPocketBase } from "./pocketbase-typegen";
 
-export const pb = new PocketBase("http://127.0.0.1:8090/") as TypedPocketBase
+export const pb = new PocketBase(
+  import.meta.env.VITE_URL_POCKETBASE
+) as TypedPocketBase;
---
+++ src/env.d.ts
@@ -0,0 +1,9 @@
+import type { TypedPocketBase } from "./pocketbase-typegen";
+
+declare global {
+    namespace App {
+        interface Locals {
+            pb: TypedPocketBase
+    }
+}
+}
