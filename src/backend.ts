2122f7af4144fe83e4250278bb86ca7ffddbe1b9
marinameloni <marina.meloni@edu.univ-fcomte.fr>
2025-03-10 13:30:52 +0100

    test

--- src/backend.ts
+++
@@ -1,6 +0,0 @@
-import PocketBase from "pocketbase";
-import type { TypedPocketBase } from "./pocketbase-typegen";
-
-export const pb = new PocketBase(
-  import.meta.env.VITE_URL_POCKETBASE
-) as TypedPocketBase;
